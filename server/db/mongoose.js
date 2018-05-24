5036dc0fab6d4cb866d1054d19a73edc259f145c
Fawaz-Khayat <fawwaz.khayyat@uoit.net>
2018-05-24 19:53:26 -0400

    fix the username for mlab database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,12 +2,24 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-let db = {
+let database = {
     localhost: 'mongodb://localhost:27017/TodoApp' ,
-    mlab: 'mongodb://fawwaz:671lHRdxRQWA@ds155644.mlab.com:55644/todoapp'
+    mlab: 'mongodb://heroku:1234@ds155644.mlab.com:55644/todoapp'
 }
-mongoose.connect(db.mlab,{
+mongoose.connect(database.localhost || database.mlab,{
     useMongoClient:true
-});
+})
+    .then(()=>{
+        console.log('---------------------------');
+        console.log('connection to mlab succeded');
+        console.log('---------------------------');
+
+    })
+    .catch((error)=>{
+        console.log('---------------------------');
+        console.log('connection to mlab failded ');
+        console.log(error);
+        console.log('---------------------------');
+    });
 
 module.exports = {mongoose};
