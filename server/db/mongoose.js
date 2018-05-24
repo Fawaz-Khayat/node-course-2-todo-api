3bc322f5858bd4ce4ea2cd60a9f627cebf598930
Fawaz-Khayat <fawwaz.khayyat@uoit.net>
2018-05-24 18:54:59 -0400

    add useMongoClient:true to mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,6 +6,8 @@ let db = {
     localhost: 'mongodb://localhost:27017/TodoApp' ,
     mlab: 'mongodb://fawwaz:671lHRdxRQWA@ds155644.mlab.com:55644/todoapp'
 }
-mongoose.connect('mongodb://fawwaz:671lHRdxRQWA@ds155644.mlab.com:55644/todoapp');
+mongoose.connect(db.localhost,{
+    useMongoClient:true
+});
 
 module.exports = {mongoose};
