7c608e314e48f868e2e3ad849d52c3abd8a9ea11
Fawaz-Khayat <fawwaz.khayyat@uoit.net>
2018-05-23 22:33:39 -0400

    GET todos/id, add check for unfound document

--- server/server.js
+++ server/server.js
@@ -44,6 +44,10 @@ app.get('/todos/:id', (req, res)=>{
     }
      Todo.findById(id)
         .then((todo)=>{
+            if (!todo){
+                return res.status(404).send();
+            }
+            
             res.send({todo});
         },(error)=>{
             res.status(400).send();
