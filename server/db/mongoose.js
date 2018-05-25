3b58c095173adf027f33f6331d511fa43b7624d9
Fawaz-Khayat <fawwaz.khayyat@uoit.net>
2018-05-24 20:05:35 -0400

    assign the choice of db url to const in mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,22 +2,25 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-let database = {
+let db = {
     localhost: 'mongodb://localhost:27017/TodoApp' ,
     mlab: 'mongodb://heroku:1234@ds155644.mlab.com:55644/todoapp'
 }
-mongoose.connect(database.localhost || database.mlab,{
+
+const db_url = db.localhost || db.mlab ;
+mongoose.connect(db_url,{
     useMongoClient:true
 })
     .then(()=>{
         console.log('---------------------------');
-        console.log('connection to mlab succeded');
+        console.log('connection succeded');
+        console.log('connected to ', db_url);
         console.log('---------------------------');
 
     })
     .catch((error)=>{
         console.log('---------------------------');
-        console.log('connection to mlab failded ');
+        console.log('connection failded ');
         console.log(error);
         console.log('---------------------------');
     });
